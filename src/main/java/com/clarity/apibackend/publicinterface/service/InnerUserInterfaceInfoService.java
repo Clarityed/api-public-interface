7a7bece9e917ada91a8821e72962288683d6137c
clarity <2270893459@qq.com>
2023-10-15 13:08:16 +0800

    add 调用的接口剩余次数为 0 ，无法调用的功能

--- src/main/java/com/clarity/apibackend/publicinterface/service/InnerUserInterfaceInfoService.java
+++ src/main/java/com/clarity/apibackend/publicinterface/service/InnerUserInterfaceInfoService.java
@@ -14,7 +14,16 @@ public interface InnerUserInterfaceInfoService {
      *
      * @param interfaceInfoId 接口信息 id
      * @param userId          用户 id
-     * @return 1 - 表示调用成功，0 - 表示调用失败
+     * @return true - 表示调用成功，false- 表示调用失败
      */
     boolean invokeCount(long interfaceInfoId, long userId);
+
+    /**
+     * 判断调用接口的剩余次数
+     *
+     * @param interfaceInfoId 接口信息 id
+     * @param userId          用户 id
+     * @return true - 还有次数，false - 表示没用次数
+     */
+    boolean judgeInterfaceInfoLeftNum(long interfaceInfoId, long userId);
 }
